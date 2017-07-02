b396132c0ac8884cf97bfe1e86be45976776378c
lglfa <lglfa666@gmail.com>
2017-07-02 19:08:04 +0800

    第一次出现error后等待时间更改

--- ChinaAQI.py
+++ ChinaAQI.py
@@ -20,6 +20,8 @@ def getProvince(City):
 def dataToSQL():
 	lastTime = ''
 	now_time = '1900/00/00 00:00:00'
+	stop = 0
+	time_compensate = 0
 	while True:
 
 		start_time = time.time()
@@ -38,15 +40,25 @@ def dataToSQL():
 			url = 'http://www.pm25.in/api/querys/all_cities.json?token=5j1znBVAsnSf5xQyNQyq'
 			resp = urllib2.urlopen(url)
 			data = json.loads(resp.read())
-			if 'error' in data:
-				print 'Sorry, API is not available in this hour.', (' Wait for %s seconds' % time_check)
+			#如果第一次请求失败尝试5min后再次请求
+			if len(data)<100 and stop == 0:
+				print 'Sorry, API error.', 'Try again in 5 min. ', now_time
+				stop = 1
+				time_compensate = 300
+				time.sleep(300)
+				continue
+			#如果第二次还失败的话就加上刚才的时间等1小时
+			if len(data)<100 and stop == 1:
+				print 'Sorry, API is not available in this hour.', (' Wait for %s seconds' % time_check), now_time
 				##记录丢失的数据
 				with open('missingDataLog.txt', 'a') as f:
-					missingTime = datetime.datetime.strptime(lastTime, '%Y/%m/%d %H:%M:%S') + datetime.timedelta(hours=1)
+					missingTime = datetime.datetime.strptime(now_time, '%Y/%m/%d %H:%M:%S') - datetime.timedelta(hours=1)
+					missingTime = missingTime.strftime('%Y/%m/%d %H:00:00')
 					f.write(str(missingTime) + '\n')
-				##再等一小时吧
-				time.sleep(time_check)
+				stop = 0
+				time.sleep(time_check -299)
 				continue
+
 		# Pandas dataframe 处理
 			column = [u'time_point', u'area', u'position_name', u'pm2_5', u'pm10', u'co',u'no2', u'o3', u'so2', u'o3_8h', u'pm2_5_24h', u'pm10_24h', u'co_24h', u'no2_24h', u'o3_24h', u'so2_24h',  u'o3_8h_24h', u'aqi', u'quality',  u'primary_pollutant', u'station_code',u'province']
 			frame = pd.DataFrame(data,columns=column)
@@ -56,6 +68,7 @@ def dataToSQL():
 			time.sleep(20)
 			continue
 		#改下时间格式
+		stop = 0
 		DateTime = dateutil.parser.parse(modeTime)
 		DateTime = DateTime.strftime('%Y/%m/%d %H:%M:%S')
 		frame['time_point'] = DateTime
@@ -64,7 +77,7 @@ def dataToSQL():
 		if DateTime == lastTime:
 			print 'No change for the latest check at', now_time+'.', ('Wait for %s seconds' % time_check)
 			processTime = time.time() - start_time
-			time.sleep(time_check-processTime)
+			time.sleep(time_check-processTime-time_compensate)
 			continue
 
 		#把对应省份加到dataframe
@@ -86,12 +99,12 @@ def dataToSQL():
 		processTime = time.time() - start_time
 		print DateTime, '   ', ("Processed in  %.2f seconds at" % processTime), now_time
 
-		time.sleep(time_check-processTime)
+		time.sleep(time_check-processTime-time_compensate)
 
+		time_compensate = 0
 #######################################  Main Body #############################################
 if __name__ == "__main__":
 
-	#数据库名和上面的北京那个表的数据库不要忘记哦，还有上面的API key
 	engine = create_engine('', encoding='utf-8')
 
 	with open('city_province.json') as f:
