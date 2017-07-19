7af36cd33e694624b263397629ba1a3022c532bb
lglfa <lglfa666@gmail.com>
2017-07-19 23:41:50 +0800

    版本2加try

--- ChinaAQI_v2.py
+++ ChinaAQI_v2.py
@@ -24,7 +24,6 @@ def dataToSQL():
 	now_time = '1900-00-00 00:00'
 	fail = 0
 	time_error = 0
-	time_compensate = 0
 	time_severerror = 0
 
 	while True:
@@ -34,7 +33,7 @@ def dataToSQL():
 
 		#如果服务器死掉，run again
 		try:
-			engine = create_engine('',encoding='utf-8')
+			engine = create_engine('mysql+mysqldb://AerosolRoot:Passw0rd@139.159.221.133/chinaaqi?charset=utf8',encoding='utf-8')
 		except:
 			print 'Server may not be available, try again in 1 min'
 			time.sleep(60)
@@ -57,17 +56,15 @@ def dataToSQL():
 			data = json.loads(resp.read())
 			#如果第一次请求失败尝试5min后再次请求
 			if len(data)<100 and fail == 0:
-				print 'Sorry, API error.', 'Try again in 5 min. ', now_time
+				print 'Sorry, API error.', 'Try again in 2 min. ', now_time
 				fail = fail + 1
-				time_compensate = 300
-				time.sleep(300)
+				time.sleep(120)
 				continue
 			# 如果第二次请求失败尝试5min后再次请求
 			if len(data) < 100 and fail == 1:
-				print 'Sorry, API error.', 'Try twice in 5 min. ', now_time
+				print 'Sorry, API error.', 'Try twice in 2 min. ', now_time
 				fail = fail + 1
-				time_compensate = 600
-				time.sleep(300)
+				time.sleep(120)
 				continue
 			#如果第三次还失败的话就加上刚才的时间等1小时
 			if len(data)<100 and fail == 2:
@@ -78,8 +75,7 @@ def dataToSQL():
 					missingTime = missingTime.strftime('%Y/%m/%d %H:00:00')
 					f.write(str(missingTime) + '\n')
 				fail = 0
-				time_compensate = 0
-				time.sleep(time_check -600)
+				time.sleep(time_check)
 				continue
 		# Pandas dataframe 处理
 			column = [u'time_point', u'area', u'position_name', u'pm2_5', u'pm10', u'co',u'no2', u'o3', u'so2', u'o3_8h', u'pm2_5_24h', u'pm10_24h', u'co_24h', u'no2_24h', u'o3_24h', u'so2_24h',  u'o3_8h_24h', u'aqi', u'quality',  u'primary_pollutant', u'station_code',u'province']
@@ -91,43 +87,47 @@ def dataToSQL():
 			time_error = time.time() - start_time + time_error
 			continue
 
-		#改下dataframe时间格式
-		DateTime = dateutil.parser.parse(modeTime)
-		DateTime = DateTime.strftime('%Y/%m/%d %H:%M:%S')
-		frame['time_point'] = DateTime
-		#把对应省份加到dataframe
-		allProvince = [getProvince(i) for i in frame['area']]
-		provinceList = ['NA' if v is None else v for v in allProvince]
-		frame['province'] = provinceList
+		try:
+			#改下dataframe时间格式
+			DateTime = dateutil.parser.parse(modeTime)
+			DateTime = DateTime.strftime('%Y/%m/%d %H:%M:%S')
+			frame['time_point'] = DateTime
+			#把对应省份加到dataframe
+			allProvince = [getProvince(i) for i in frame['area']]
+			provinceList = ['NA' if v is None else v for v in allProvince]
+			frame['province'] = provinceList
 
-		#如果数据没有变化就不发数据库，等待一段时间继续check
-		if DateTime == lastTime:
-			print 'No change for the latest check at', now_time+'.', ('Wait for %s seconds' % time_check)
+			#如果数据没有变化就不发数据库，等待一段时间继续check
+			if DateTime == lastTime:
+				print 'No change for the latest check at', now_time+'.', ('Wait for %s seconds' % time_check)
+				processTime = time.time() - start_time
+				time.sleep(time_check-processTime)
+				continue
+
+			#要发往数据库的 31个省份
+			with open('province.txt') as f:
+				string = f.readlines()
+				province_sql = [i.decode('utf-8').strip() for i in string]
+			del province_sql[0]
+			# 数据按照省份发往数据库
+			for i in province_sql:
+				t = frame[frame['province'].isin([i])]
+				t.to_sql(i, con=engine, if_exists='append', chunksize=5000, index=False)
+
+			#总共的处理时间，并print
 			processTime = time.time() - start_time
-			time.sleep(time_check-processTime-time_compensate)
+			print DateTime, '   ', ("Processed in  %.2f seconds at" % processTime), now_time
+			#严格等待下一个时间间隔
+			time.sleep(time_check-processTime-time_error-time_severerror)
+
+			#参数归0
+			fail = 0
+			time_error = 0
+			time_severerror = 0
+		except:
+			print 'some error...'
+			time.sleep(20)
 			continue
-
-		#要发往数据库的 31个省份
-		with open('province.txt') as f:
-			string = f.readlines()
-			province_sql = [i.decode('utf-8').strip() for i in string]
-		del province_sql[0]
-		# 数据按照省份发往数据库
-		for i in province_sql:
-			t = frame[frame['province'].isin([i])]
-			t.to_sql(i, con=engine, if_exists='append', chunksize=5000, index=False)
-
-		#总共的处理时间，并print
-		processTime = time.time() - start_time
-		print DateTime, '   ', ("Processed in  %.2f seconds at" % processTime), now_time
-		#严格等待下一个时间间隔
-		time.sleep(time_check-processTime-time_compensate-time_error-time_severerror)
-
-		#参数归0
-		fail = 0
-		time_compensate = 0
-		time_error = 0
-		time_severerror = 0
 #######################################  Main Body #############################################
 if __name__ == "__main__":
 
